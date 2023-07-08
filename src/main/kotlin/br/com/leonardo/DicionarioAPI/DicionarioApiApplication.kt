2e1cd5df9532a7df1c415e8f20c982f7161d23ac
leona <leonardo.tissi.si@gmail.com>
2023-07-08 12:00:36 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/leonardo/DicionarioAPI/DicionarioApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.leonardo.DicionarioAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DicionarioApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DicionarioApiApplication>(*args)
+}
