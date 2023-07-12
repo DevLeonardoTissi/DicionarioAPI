9239cd4240f273679f17b5d01195cdb035e9b46d
leona <leonardo.tissi.si@gmail.com>
2023-07-11 23:51:22 -0300

    adição métodos sentence

--- src/main/kotlin/br/com/leonardo/dicionarioAPI/service/SentencesService.kt
+++ src/main/kotlin/br/com/leonardo/dicionarioAPI/service/SentencesService.kt
@@ -1,11 +1,22 @@
 package br.com.leonardo.dicionarioAPI.service
 
 import br.com.leonardo.dicionarioAPI.dto.SentenceView
+import br.com.leonardo.dicionarioAPI.utils.connectsWithTheUrlAndReturnsTheDocument
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 import org.springframework.stereotype.Service
 
 @Service
 class SentencesService {
     fun searchSentences(word: String): List<SentenceView> {
-        return mutableListOf()
+        val doc: Document = connectsWithTheUrlAndReturnsTheDocument(word)
+        val sentencesElements: Elements = doc.select(".frase span ")
+
+        return sentencesElements.map { element: Element ->
+            val sentence = element.ownText().trim()
+            val author = element.selectFirst("em")?.text()?.replace("-", "")?.trim()
+            SentenceView(sentence = sentence, author = author)
+        }
     }
 }
