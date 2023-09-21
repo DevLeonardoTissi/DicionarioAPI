8fdb116b408f4ef390c4314e6ba184ab7efa020c
leona <leonardo.tissi.si@gmail.com>
2023-09-21 12:28:43 -0300

    trying to pass context text

--- src/test/kotlin/br/com/leonardo/dicionarioAPI/DicionarioApiApplicationTests.kt
+++ src/test/kotlin/br/com/leonardo/dicionarioAPI/DicionarioApiApplicationTests.kt
@@ -1,10 +1,13 @@
 package br.com.leonardo.dicionarioAPI
 
+import br.com.leonardo.dicionarioAPI.configuration.DatabaseContainerConfiguration
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
-class DicionarioApiApplicationTests {
+@Testcontainers
+class DicionarioApiApplicationTests: DatabaseContainerConfiguration() {
 
 	@Test
 	fun contextLoads() {
