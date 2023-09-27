578c1c2de0f6ac494b7740646422853d70cbfbbd
leona <leonardo.tissi.si@gmail.com>
2023-09-27 19:01:58 -0300

    refactor user-test migration

--- src/main/resources/db/migration/V4__create_user.sql
+++ src/main/resources/db/migration/V4__create_user.sql
@@ -1,1 +1,1 @@
-INSERT INTO user VALUES (1, 'leonardo', 'leonardo_spring_boot@gmail.com', '$2y$10$cU0Xqq/thVsHrmEfSUHLse22pydsKKgEF1n5juSQDeTRnmsTkfgL2')
+INSERT INTO user VALUES (1, 'leonardo', 'leonardo_spring_boot_api_test@email.com', '$2y$10$cU0Xqq/thVsHrmEfSUHLse22pydsKKgEF1n5juSQDeTRnmsTkfgL2')
